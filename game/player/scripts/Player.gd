0b1be229ebcb447eb10fca3a464ed2923dd21255
psbrunosouza <psbrunosouza@gmail.com>
2023-02-01 00:19:32 -0300

    feat: wall jump

--- game/player/scripts/Player.gd
+++ game/player/scripts/Player.gd
@@ -1,6 +1,6 @@
 extends PlayerModel
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	gravity(delta)
 	
 	idle()
@@ -10,31 +10,32 @@ func _physics_process(delta):
 	
 	motion = move_and_slide(motion, Vector2.UP)
 	
-func gravity(delta):
-	if(motion.y <= 0):
+func gravity(delta) -> void:
+	if(next_to_wall() && direction != 0 && motion.y >= 0):
+		motion.y += WALL_ATTACHED_GRAVITY * delta
+	elif(motion.y < 0):
 		motion.y += JUMP_GRAVITY * delta
-	else:
+	elif(motion.y >= 0):
 		motion.y += FALL_GRAVITY * delta 
-	
-	if(!is_jumping && next_to_wall() && direction != 0):
-		motion.y = WALL_ATTACHED_GRAVITY
 		
-func idle(): 
+func idle() -> void: 
 	if(is_on_floor()):
 		is_jumping = false
 		performed_jumps = 0
 		animation_player_machine.travel("idle")
 	
-func movement(): 
+func movement() -> void: 
 	direction = Input.get_action_strength("right") - Input.get_action_strength("left")
 	if(direction != 0): 
 		motion.x = lerp(motion.x, direction * MOVE_SPEED, 0.2)
 		sprite.scale.x = direction;
 		animation_player_machine.travel("run")
+	elif(direction == 0 && !is_on_floor()):
+		motion.x = lerp(motion.x, 0, 0.15)
 	else:
-		motion.x = lerp(motion.x, 0, 0.3)
+		motion.x = lerp(motion.x, 0, 0.4)
 		
-func wall_jump():
+func wall_jump() -> void:
 	if(
 		Input.is_action_just_pressed("jump") 
 		&& !is_on_floor() 
@@ -52,7 +53,7 @@ func wall_jump():
 		motion.x = WALL_IMPULSE_SPEED
 		performed_jumps += 1
 		
-func jump():
+func jump() -> void:
 	if(Input.is_action_just_pressed("jump") && is_on_floor()):
 		performed_jumps += 1;
 		if(performed_jumps <= MAX_JUMP_QUANTITY):
--- game/player/scripts/PlayerModel.gd
+++ game/player/scripts/PlayerModel.gd
@@ -4,7 +4,7 @@ class_name PlayerModel
 
 const FALL_GRAVITY: int = 800
 const JUMP_GRAVITY: int = 600
-const WALL_ATTACHED_GRAVITY: int = 30
+const WALL_ATTACHED_GRAVITY: int = 50
 const JUMP_FORCE: int = -250
 const WALL_IMPULSE_HEIGHT = -250
 const MOVE_SPEED: int = 150
@@ -22,6 +22,6 @@ onready var sprite: Sprite = $Sprite
 onready var animation_player_tree: AnimationTree = $AnimationMachinePlayer
 onready var animation_player_machine = animation_player_tree.get("parameters/playback")
 
-func next_to_wall():
+func next_to_wall() -> bool:
 	return wall_detector_right.is_colliding() || wall_detector_left.is_colliding()
 
